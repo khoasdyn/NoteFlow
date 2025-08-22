5d740a5fed6f804fe3f9f5527846316138937b78
Liam <khoasdyn@gmail.com>
2025-08-22 11:08:29 +0700

    Initial Commit

---
+++ NoteFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoteFlow
+//
+//  Created by khoasdyn on 22/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoteFlow/NoteFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteFlowApp.swift
+//  NoteFlow
+//
+//  Created by khoasdyn on 22/8/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
