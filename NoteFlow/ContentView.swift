17582c8c5500e479a5fa29af5d8119c951fee1f5
Liam <khoasdyn@gmail.com>
2025-08-22 13:50:24 +0700

    Added button sample data

--- NoteFlow/ContentView.swift
+++ NoteFlow/ContentView.swift
@@ -9,37 +9,53 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var context
+    @Environment(\.modelContext) private var modelContext
+    @Query var notes: [Note]
     
     let columns = [GridItem(.adaptive(minimum: 200), spacing: 16)]
-
+    
     var body: some View {
-        GeometryReader { geometry in
-            ScrollView(.vertical) {
-                LazyVGrid(columns: columns, spacing: 16) {
-                    CardView(note: Note(title: "Grocery List for Weekend Shopping"))
-                    CardView(note: Note(title: "Hello World"))
-                    CardView(note: Note(title: "Notes from Monday Team Meeting"))
-                    CardView(note: Note(title: "Notes from Monday Team Meeting"))
-                    CardView(note: Note(title: "Summary of Monday Team Meeting Action Items"))
-                    CardView(note: Note(title: "Follow-up Tasks from Monday Team Meeting"))
-                    CardView(note: Note(title: "Decisions Made During Monday Team Meeting"))
-                    CardView(note: Note(title: "Attendees and Absentees - Monday Team Meeting"))
-                    CardView(note: Note(title: "Monday Team Meeting: Discussion Points"))
-                    CardView(note: Note(title: "Monday Team Meeting: Next Steps"))
-                    CardView(note: Note(title: "Monday Team Meeting: Open Questions"))
-                    CardView(note: Note(title: "Monday Team Meeting: Feedback and Suggestions"))
-                    CardView(note: Note(title: "Monday Team Meeting: Deadlines and Deliverables"))
-                    CardView(note: Note(title: "Monday Team Meeting: Miscellaneous Notes"))
-                    CardView(note: Note(title: "Notes from Monday Team Meeting"))
-                    CardView(note: Note(title: "Brainstorming Project Ideas for Q3"))
-                    CardView(note: Note(title: "Personal Information"))
-                    CardView(note: Note(title: "Detailed Travel Plans for Japan Trip"))
-                }
-                .padding(16)
+        ScrollView(.vertical) {
+            Button("Add Sample") {
+                addSampledNotes()
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.white)
+            .buttonStyle(.borderedProminent)
+            
+            LazyVGrid(columns: columns, spacing: 16) {
+                ForEach(notes, id: \.id) { note in
+                    CardView(note: note)
+                }
+            }
+            .padding(16)
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.white)
+    }
+    
+    func addSampledNotes() {
+        let titles = [
+            "Grocery List for Weekend Shopping",
+            "Hello World",
+            "Notes from Monday Team Meeting",
+            "Notes from Monday Team Meeting",
+            "Summary of Monday Team Meeting Action Items",
+            "Follow-up Tasks from Monday Team Meeting",
+            "Decisions Made During Monday Team Meeting",
+            "Attendees and Absentees - Monday Team Meeting",
+            "Monday Team Meeting: Discussion Points",
+            "Monday Team Meeting: Next Steps",
+            "Monday Team Meeting: Open Questions",
+            "Monday Team Meeting: Feedback and Suggestions",
+            "Monday Team Meeting: Deadlines and Deliverables",
+            "Monday Team Meeting: Miscellaneous Notes",
+            "Notes from Monday Team Meeting",
+            "Brainstorming Project Ideas for Q3",
+            "Personal Information",
+            "Detailed Travel Plans for Japan Trip"
+        ]
+        for title in titles {
+            modelContext.insert(Note(title: title))
         }
     }
 }
@@ -54,7 +70,6 @@ struct CardView: View {
                 .foregroundColor(.yellow900)
                 .lineLimit(3)
             Text(note.content)
-                .multilineTextAlignment(.leading)
                 .font(.system(size: 16, weight: .regular))
                 .foregroundColor(.grayWarm500)
                 .lineLimit(4)
@@ -64,7 +79,7 @@ struct CardView: View {
         .frame(minHeight: 200)
         .frame(maxWidth: .infinity)
         .background(.yellow100)
-        .clipShape(RoundedRectangle(cornerRadius: 8))
+        .clipShape(RoundedRectangle(cornerRadius: 12))
     }
 }
 
